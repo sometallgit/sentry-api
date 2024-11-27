87e23b28b5d8bedbb9c7e85ef6dd284ccd92a5db
Cameron Bradley <cameron.bradley@emesent.io>
2024-11-27 20:37:16 +1000

    linter changes

--- sentry_analytics/sentry_api.py
+++ sentry_analytics/sentry_api.py
@@ -10,26 +10,26 @@ class sentry_api_builder():
 	
 
 	@classmethod
-	def get_projects(self, org_slug: str) -> str:
-		return f"{self.endpoint_baseurl}{self.organisations}{org_slug}/{self.projects}"
+	def get_projects(cls, org_slug: str) -> str:
+		return f"{cls.endpoint_baseurl}{cls.organisations}{org_slug}/{cls.projects}"
 
 	@classmethod
-	def get_issue_event(self, issue_id: str, event_id: str) -> str:
+	def get_issue_event(cls, issue_id: str, event_id: str) -> str:
 		# https://sentry.io/api/0/issues/{issue_id}/events/{event_id}/
-		return f"{self.endpoint_baseurl}{self.issues}{issue_id}/{self.events}{event_id}"
+		return f"{cls.endpoint_baseurl}{cls.issues}{issue_id}/{cls.events}{event_id}"
 
 	@classmethod
-	def get_issue_events(self, issue_id: str) -> str:
+	def get_issue_events(cls, issue_id: str) -> str:
 		# /api/0/issues/{issue_id}/events/
-		return f"{self.endpoint_baseurl}{self.issues}{issue_id}/{self.events}" #?&cursor=0:100:0
+		return f"{cls.endpoint_baseurl}{cls.issues}{issue_id}/{cls.events}" #?&cursor=0:100:0
 
 class Sentry_Api():
 	def __init__(self, auth_token:str) -> None:
 		self.auth_token:str = auth_token
 
-	def build_header(self,custom_header):
-		header: dict = {}
-		auth_header: dict = {"Authorization": f"Bearer {self.auth_token}"}
+	def build_header(self, custom_header):
+		header: dict[str, str] = {}
+		auth_header: dict[str, str] = {"Authorization": f"Bearer {self.auth_token}"}
 		header.update(auth_header)
 		if custom_header:
 			header.update(custom_header)
